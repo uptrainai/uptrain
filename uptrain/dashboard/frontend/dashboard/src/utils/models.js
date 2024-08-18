4e9ca1dc35af885ade413bdb93499f3063c7d822
rahulkrprajapati <rahul.8981513581@gmail.com>
2024-08-18 19:00:08 +0530

    adding support for gpt-4o-mini

--- uptrain/dashboard/frontend/dashboard/src/utils/models.js
+++ uptrain/dashboard/frontend/dashboard/src/utils/models.js
@@ -2,6 +2,7 @@ export const models = {
   "gpt-3.5-turbo": ["openai_api_key"],
   "gpt-4": ["openai_api_key"],
   "gpt-4-turbo-preview": ["openai_api_key"],
+  "gpt-4o-mini": ["openai_api_key"],
   "claude-2.1": ["anthropic_api_key"],
   "azure-deployment": [
     "azure_api_base",
